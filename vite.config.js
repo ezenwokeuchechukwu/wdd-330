72c5add63cbc2efa3b7263e2eaca54911e51e018
ezenwokeuchechukwu <ezenwokeuchechukwu@gmail.com>
2025-05-15 14:54:24 +0100

    completed

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { resolve } from "path";
+import { resolve } from 'path';
 
 export default {
+  root: 'src', // Tell Vite to serve from src/
   build: {
-    outDir: "../dist",
+    outDir: '../dist', // Output outside the root
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
+        main: resolve(__dirname, 'src/index.html'),
+        cart: resolve(__dirname, 'src/cart/index.html'),
+        checkout: resolve(__dirname, 'src/checkout/index.html'),
+        product: resolve(__dirname, 'src/product_pages/index.html'),
       },
     },
   },
